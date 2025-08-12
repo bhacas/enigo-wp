4afb17fb39d7f45a17552fb70cf254882856d310
Bartlomiej Hacas <bhacas@gmail.com>
2025-08-12 12:12:39 +0200

    init

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
                 }
                 ?>
             </nav>
-           <div class="flex space-x-4"><a href="#" class="text-gray-400 hover:text-white transition-colors">
+           <div class="flex space-x-4"><a href="https://www.facebook.com/profile.php?id=61579066572531" target="_blank" class="text-gray-400 hover:text-white transition-colors">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                          class="lucide lucide-facebook">
