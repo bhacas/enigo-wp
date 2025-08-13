9a9b55d03f6442b9a43221c9435e98cf7e539056
Bartlomiej Hacas <bhacas@gmail.com>
2025-08-13 21:23:33 +0200

    cleanup

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header();  ?>
+
+    <div class="w-full bg-gray-50 py-16">
+        <div class="container mx-auto px-4 text-center">
+            <h1 class="text-3xl md:text-5xl font-bold text-gray-900 mb-4">
+                404</h1>
+            <p class="text-lg text-gray-600 max-w-2xl mx-auto">
+                Nie znaleziono strony, której szukasz. Możliwe, że została usunięta lub przeniesiona na inny adres.
+            </p>
+        </div>
+    </div>
+
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<?php get_header();  ?>
+<?php get_header();
 
-    <div class="w-full bg-gray-50 py-16">
-        <div class="container mx-auto px-4 text-center">
-            <h1 class="text-3xl md:text-5xl font-bold text-gray-900 mb-4">
-                404</h1>
-            <p class="text-lg text-gray-600 max-w-2xl mx-auto">
-                Nie znaleziono strony, której szukasz. Możliwe, że została usunięta lub przeniesiona na inny adres.
-            </p>
-        </div>
-    </div>
+global $wp_query;
+$wp_query->set_404();
+status_header( 404 );
+nocache_headers();
+include( get_query_template( '404' ) );
+exit;
 
-
-<?php get_footer(); ?>
+?>
